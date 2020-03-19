e82f12ce0a2772938a95662ac61c286b659669c1
Liam K <treehouseliam@gmail.com>
2020-03-18 23:56:13 -0700

    Finished project no idea if it works off machine

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bird-visual-reference
-An application to open and use 
+An application to look at photos of birds
+The application looks good but doesn't have all the photos available for all the birds.
+The test birds I chose are good looking ;) 
+I hope you feel better Joe I'll see you next term
+
---
+++ dictionary.py
@@ -0,0 +1,59 @@
+#!//usr/bin/python3
+"""Dictionary with the names of all the photographed birds in the database
+"""
+from PIL import Image
+from pathlib import Path
+import sys
+
+def main():
+        print("In our beta stages we are only offering a few birds at the moment: \
+            \n Alexandrine Parakeet, American Goldfinch, \
+            \n Black Swan ,Curl Crested Aracuri,\
+            \n Quetzal and, Rainbow Lorikeet.")
+        user_bird = input("Which would you like to see? q to quit ")
+        if user_bird == 'q':
+            sys.exit()
+        user_photo = input("Each bird has 5 photos take your pick! q to quit ")
+        if user_photo == 'q':
+            sys.exit()
+        user_choice = birds[user_bird][user_photo]
+        img_appear = Image.open(user_choice)
+        try:
+            img_appear.show()
+        except IOError:
+            pass  
+
+birds = {'American Goldfinch': {'1': Path('/Bird-visual-reference/test case birbs/AMERICAN GOLDFINCH/1.jpg'), 
+                                '2': Path('/Bird-visual-reference/test case birbs/AMERICAN GOLDFINCH/2.jpg'),
+                                '3': Path('/Bird-visual-reference/test case birbs/AMERICAN GOLDFINCH/3.jpg'),
+                                '4': Path('/Bird-visual-reference/test case birbs/AMERICAN GOLDFINCH/4.jpg'),
+                                '5': Path('/Bird-visual-reference/test case birbs/AMERICAN GOLDFINCH/5.jpg')},
+ 'Alexandrine Parakeet': {'1':'/Bird-visual-reference/test case birbs/ALEXANDRINE PARAKEET',
+                         '2': '/Bird-visual-reference/test case birbs/ALEXANDRINE PARAKEET/2.jpg', 
+                         '3': '/Bird-visual-reference/test case birbs/ALEXANDRINE PARAKEET/3.jpg', 
+                         '4': '/Bird-visual-reference/test case birbs/ALEXANDRINE PARAKEET/4.jpg', 
+                         '5': '/Bird-visual-reference/test case birbs/ALEXANDRINE PARAKEET/5.jpg'}, 
+ 'Black Swan': {'1': '/Bird-visual-reference/test case birbs/BLACK SWAN/1.jpg',
+                '2': '/Bird-visual-reference/test case birbs/BLACK SWAN/2.jpg',
+                '3': '/Bird-visual-reference/test case birbs/BLACK SWAN/3.jpg',
+                '4': '/Bird-visual-reference/test case birbs/BLACK SWAN/4.jpg',
+                '5': '/Bird-visual-reference/test case birbs/BLACK SWAN/5.jpg'},
+ 'Curl Crested Aracuri': {'1': '/Bird-visual-reference/test case birbs/CURL CRESTED ARACURI/1.jpg',
+                        '2': '/Bird-visual-reference/test case birbs/CURL CRESTED ARACURI/2.jpg',
+                        '3': '/Bird-visual-reference/test case birbs/CURL CRESTED ARACURI/3.jpg',
+                        '4': '/Bird-visual-reference/test case birbs/CURL CRESTED ARACURI/4.jpg',
+                        '5': '/Bird-visual-reference/test case birbs/CURL CRESTED ARACURI/5.jpg'},
+ 'Quetzal': {'1': '~/Bird-visual-reference/test case birbs/QUETZAL/1.jpg',
+            '2': '~/Bird-visual-reference/test case birbs/QUETZAL/2.jpg',
+            '3': '~/Bird-visual-reference/test case birbs/QUETZAL/3.jpg',
+            '4': '~/Bird-visual-reference/test case birbs/QUETZAL/4.jpg',
+            '5': '~/Bird-visual-reference/test case birbs/QUETZAL/5.jpg'},
+ 'Rainbow Lorikeet': {'1': '~/Bird-visual-reference/test case birbs/RAINBOW LORIKEET/1.jpg', 
+                    '2': '~/Bird-visual-reference/test case birbs/RAINBOW LORIKEET/2.jpg', 
+                    '3': '~/Bird-visual-reference/test case birbs/RAINBOW LORIKEET/3.jpg', 
+                    '4': '~/Bird-visual-reference/test case birbs/RAINBOW LORIKEET/4.jpg', 
+                    '5': '~/Bird-visual-reference/test case birbs/RAINBOW LORIKEET/5.jpg'}}
+
+if __name__ == "__main__":
+    main()
+    
